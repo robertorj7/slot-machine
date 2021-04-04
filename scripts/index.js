1e43c6885e90d7b1a9ad91fddd3cf9448bacdb29
Roberto R Jr <rrjr87@outlook.com>
2021-04-04 19:17:34 -0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,73 @@ const items = [
     src: './assets/img/bomb.png',
   },
 ];
+
+const balance = document.getElementById('balance');
+const buttonSpin = document.getElementById('spin-button');
+const title = document.querySelector('.title');
+
+balance.innerHTML = availableToBet;
+
+buttonSpin.addEventListener('click', () => {
+  let selectedHeroesIds = [];  
+  const slotElements = document.querySelectorAll('.slot');
+
+  slotElements.forEach((slot) => {
+    slot.classList.add('rotate');
+    setTimeout(() => {
+      slot.classList.remove('rotate');
+    }, 500);
+  })
+    
+  for (i = 1; i < 4; i++) {
+    const slot = document.getElementById(`slot-item-${i}`);
+    const hero = findHero(randomNumber());
+
+    slot.setAttribute('src', hero.src);
+    slot.setAttribute('title', hero.name);
+    slot.setAttribute('alt', hero.name);
+
+    selectedHeroesIds.push(hero.id);
+  }
+
+  function randomNumber() {
+    return Math.floor(Math.random() * 4 + 1);  
+  }
+
+  function findHero(id) {
+    return items.find((item) => {
+      return item.id === id;
+    })
+  }
+  
+  if (selectedHeroesIds[0] === selectedHeroesIds[1] &&
+      selectedHeroesIds[1] === selectedHeroesIds[2]
+  ) {
+    const selectedId = selectedHeroesIds[0];
+
+    const reward = items.find((item) => {
+      return item.id === selectedId;
+    }).reward;
+
+    setTimeout(() => {
+      balance.innerHTML = Number(balance.innerHTML) + reward;
+
+      if (selectedId === 1 || selectedId === 2) {
+        title.innerHTML = "You Win!";
+        resetTitle()
+      } else if (balance.innerHTML <= 0) {
+          title.innerHTML = "GAME OVER!";
+          buttonSpin.setAttribute('disabled', 'disabled');
+        } else {
+            title.innerHTML = "You Lost Money!";
+            resetTitle()
+          }
+        }, 500)
+  }
+  
+  function resetTitle() {
+    setTimeout(() => {
+      title.innerHTML = "HERO MACHINE";
+    }, 1500)
+  }
+})
